422c5ba398394c7c3a355c83671fe55cdbd0c48c
Truth <henrysempire111gmail.com>
2021-07-06 02:59:27 -0700

    updated my django_project

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'password_reset'
 ]
 
 MIDDLEWARE = [
@@ -131,9 +132,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'henrysempire111@gmail.com'
-# EMAIL_Host_PASSWORD = 'vocabulary'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'henrysempire111@gmail.com'
+EMAIL_HOST_PASSWORD = 'vocabulary'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
